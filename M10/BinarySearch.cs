0867cf43bcd3feccdc5cc5950b45c8b390c00316
Wixiam <k.aziza.i@mail.ru>
2019-12-27 16:37:05 +0600

    Add project files.

---
+++ M10/BinarySearch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M10
+{
+    public class BinarySearch
+    {
+        public bool BinarySearching<T>(T item, T[] array)
+        {
+            int first = 0;
+            int last = array.Length - 1;
+            int middle = last / 2;
+
+            while (last >= first)
+            {
+                var compareResult = ((IComparable)(middle)).CompareTo(item);
+
+                if (compareResult == 0)
+                {
+                    return true;
+                }
+
+                if(compareResult > 0)
+                {
+                    last = middle - 1;
+                }
+                else
+                {
+                    first = middle+1;
+                }
+
+                middle = first + ((last - first)/2); 
+            }
+
+            return false;
+        }
+    }
+}
